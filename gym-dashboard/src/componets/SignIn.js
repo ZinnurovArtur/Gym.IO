312aca12c186220989663756e7b22399a9fda859
Arthur <zinnurov_2012@mail.ru>
2022-01-05 13:07:02 +0000

    Changed styles of login page

--- gym-dashboard/src/componets/SignIn.js
+++ gym-dashboard/src/componets/SignIn.js
@@ -15,7 +15,7 @@ export default function Login() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log(emailRef.current.value)
+    console.log(emailRef.current.value);
 
     try {
       setError("");
@@ -28,69 +28,97 @@ export default function Login() {
     setLoading(false);
   }
   return (
-<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="inline-flex items-start justify-start px-6 rounded-2xl" style={{width: 910, height: 583,}}>
-        <div className="flex space-x-6 items-start justify-start bg-white shadow-2xl rounded-2xl" style={{width: 856, height: 583,}}>
-            <div className="bg-gradient-signin bg-cover bg-center background-img-sign rounded-tl-xl rounded-bl-xl" style={{width: 414, height: 583,}}/>
-            <div className="inline-flex flex-col space-y-6 items-center justify-center py-6 pl-8 pr-5">
-                <div className="inline-flex items-center justify-center">
-                    <div className="w-19 h-full">
-                        <p className="text-3xl font-medium text-gray-400">GYM.</p>
-                    </div>
-                    <p className="text-3xl font-medium text-signin">IO</p>
-                </div>
-                <div className="flex flex-col items-start justify-start">
-                    <p className="text-2xl font-medium text-gray-600">Sign in </p>
-                    <p className="text-xl font-medium text-gray-400">Sign in to continue training :) </p>
-                </div>
-                
-                <form onSubmit = {handleSubmit}>
-                
-                <div className="w-full">
-                    <div className="flex flex-col items-start justify-start w-full px-3 border rounded-lg border-gray-300">
-                        <div className="inline-flex items-start justify-start w-full py-4">
-                        <label>
-                             <input type="email" name="email" ref={emailRef} placeholder = "Email Address" className="flex-1 text-base leading-normal text-gray-500"/>
-                             </label>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-full mt-6">
-                    <div className="flex flex-col items-start justify-start w-full px-3 border rounded-lg border-gray-300">
-                        <div className="inline-flex items-start justify-start w-full py-4">
-                        <label>
-                        <input type="password" name="password" ref={passwordRef} placeholder = "Password" className="flex-1 text-base leading-normal text-gray-500"/>
-                        </label>
-                        </div>
-                    </div>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div
+        className="inline-flex items-start justify-start px-6 rounded-2xl"
+        style={{ width: 910, height: 583 }}
+      >
+        <div
+          className="flex space-x-6 items-start justify-start bg-white shadow-2xl rounded-2xl"
+          style={{ width: 856, height: 583 }}
+        >
+          <div
+            className="bg-gradient-signin bg-cover bg-center background-img-sign rounded-tl-xl rounded-bl-xl"
+            style={{ width: 414, height: 583 }}
+          />
+          <div className="inline-flex flex-col space-y-6 items-center justify-center py-6 pl-8 pr-5">
+            <div className="inline-flex items-center justify-center">
+              <div className="w-19 h-full">
+                <p className="text-3xl font-medium text-gray-400">GYM.</p>
+              </div>
+              <p className="text-3xl font-medium text-signin">IO</p>
+            </div>
+            <div className="flex flex-col items-start justify-start">
+              <p className="text-2xl font-medium text-gray-600">Sign in </p>
+              <p className="text-xl font-medium text-gray-400">
+                Sign in to continue training :){" "}
+              </p>
+            </div>
 
-                </div>
-                
-                <button type="submit" className="inline-flex items-start justify-start w-full mt-8 px-32 py-1.5 bg-signin shadow rounded-md">
-                    <p className="text-2xl font-medium text-white">Sign In</p>
-                </button>
-                </form>
-              
+            <form onSubmit={handleSubmit}>
+              <div className="w-full">
+                <label>
+                  <input
+                    type="email"
+                    name="email"
+                    autoComplete = "email"
+                    ref={emailRef}
+                    class="block w-full px-4 py-3 mb-4 border border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-blue-500 focus:outline-none"
+                    placeholder="Email address"
+                  />
+                </label>
+              </div>
 
-                <div className="inline-flex items-center justify-center px-2.5">
-                    <p className="text-base font-medium text-gray-400">Or</p>
-                </div>
-                <div className="inline-flex space-x-2.5 items-center justify-center w-full h-12 py-2.5 bg-white shadow rounded-md">
-                    <div className="flex items-center justify-center w-8 h-8 p-0.5 bg-white">
-                        <img className="flex-1 h-full rounded-lg" src="https://via.placeholder.com/29.39665412902832x29.396934509277344"/>
-                    </div>
-                    <p className="w-3/4 h-7 text-xl font-medium text-black text-opacity-50">Continue with Google</p>
-                </div>
-                <div className="inline-flex items-start justify-start px-2.5">
-                    <p className="text-base font-medium text-gray-400">No account?   <a
+              <div className="w-full mt-4">
+                <label>
+                  <input
+                    type="password"
+                    name="password"
+                    autocomplete="current-password"
+                    ref={passwordRef}
+                    placeholder="Password"
+                    className="block w-full px-4 py-3 mb-4 border border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-blue-500 focus:outline-none"
+                  />
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="inline-flex items-start justify-start w-full mt-4 px-32 py-1.5 bg-signin shadow rounded-md hover:bg-violet-400 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300"
+              >
+                <p className="text-2xl font-medium text-white">Sign In</p>
+              </button>
+            </form>
+
+            <div className="inline-flex items-center justify-center px-2.5">
+              <p className="text-base font-medium text-gray-400">Or</p>
+            </div>
+            <div className="inline-flex space-x-2.5 items-center justify-center w-full h-12 py-2.5 bg-white shadow rounded-md">
+              <div className="flex items-center justify-center w-8 h-8 p-0.5 bg-white">
+                <img
+                  className="flex-1 h-full rounded-lg"
+                  src="https://via.placeholder.com/29.39665412902832x29.396934509277344"
+                />
+              </div>
+              <p className="w-3/4 h-7 text-xl font-medium text-black text-opacity-50">
+                Continue with Google
+              </p>
+            </div>
+            <div className="inline-flex items-start justify-start px-2.5">
+              <p className="text-base font-medium text-gray-400">
+                No account?{" "}
+                <a
                   href="/signup"
                   class="text-signin hover underline"
                   bis_skin_checked="1"
-                >Create one</a></p>
-                </div>
+                >
+                  Create one
+                </a>
+              </p>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
 }
--- gym-dashboard/src/firebase.js
+++ gym-dashboard/src/firebase.js
@@ -9,7 +9,7 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDYN46Bb5KpGFXWkLHwsBV6fnqY2gnkS24",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
