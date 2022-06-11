cc9b0e0d3d3a9d9a1630b8202c473c36443240c6
Arthur <zinnurov_2012@mail.ru>
2022-06-11 19:23:00 +0100

    Navigation list elements

--- gym-dashboard/src/componets/Navigation/navList.js
+++ gym-dashboard/src/componets/Navigation/navList.js
@@ -1,67 +1,51 @@
-import DashboardOutlined from '@mui/icons-material/DashboardOutlined';
-import Person from '@mui/icons-material/Person';
-import Forum from '@mui/icons-material/Forum';
-import Analytics from '@mui/icons-material/Analytics';
-import FolderOpen from '@mui/icons-material/FolderOpen';
-import BorderColor from '@mui/icons-material/BorderColor';
-import Search from '@mui/icons-material/Search';
+import DashboardOutlined from "@mui/icons-material/DashboardOutlined";
+import Person from "@mui/icons-material/Person";
+
+import Analytics from "@mui/icons-material/Analytics";
+import FolderOpen from "@mui/icons-material/FolderOpen";
+import BorderColor from "@mui/icons-material/BorderColor";
+import Search from "@mui/icons-material/Search";
 
 const navbarList = [
   {
     icon: Search,
-    desc: 'Search',
-    secondDesc: '',
+    desc: "Search",
+    secondDesc: "",
     badge: 0,
     subList: [],
   },
   {
     icon: DashboardOutlined,
-    desc: 'Dashboard',
-    secondDesc: '',
+    desc: "Dashboard",
+    secondDesc: "",
     badge: 0,
     subList: [],
   },
   {
     icon: Person,
-    desc: 'User',
-    secondDesc: '',
+    desc: "User",
+    secondDesc: "",
     badge: 0,
     subList: [],
   },
   {
-    icon: Forum,
-    desc: 'Forum',
-    secondDesc: 'Message from andi',
-    badge: 2,
-    subList: [
-      {
-        desc: 'chat',
-        badge: 2,
-      },
-      {
-        desc: 'reminder',
-        badge: 0,
-      },
-    ],
-  },
-  {
     icon: Analytics,
-    desc: 'Analytics',
-    secondDesc: '',
+    desc: "Body analytics",
+    secondDesc: "",
     badge: 0,
     subList: [],
   },
   {
     icon: FolderOpen,
-    desc: 'Folder',
-    secondDesc: '',
+    desc: "Exsercise list",
+    secondDesc: "",
     badge: 0,
     subList: [],
   },
   {
     icon: BorderColor,
-    desc: 'Edit',
-    secondDesc: '',
+    desc: "Edit workout",
+    secondDesc: "",
     badge: 0,
     subList: [],
   },
