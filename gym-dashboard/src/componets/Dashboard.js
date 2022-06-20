d19c8224abc4987f63aac4926ebd127684ce80a0
Arthur <zinnurov_2012@mail.ru>
2022-06-20 21:42:33 +0100

    chartConfig

--- gym-dashboard/src/componets/Dashboard.js
+++ gym-dashboard/src/componets/Dashboard.js
@@ -7,6 +7,7 @@ import Overview from "./Overview";
 import Themecolon from "./Themecolon";
 import Header from "./Header";
 import ThemeContext from "../contexts/ThemeContext";
+import Chart from "./Chart";
 
 
 const Dashboard = () => {
@@ -14,14 +15,14 @@ const Dashboard = () => {
   return (
     
     <div className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand ${
-        darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"
+        darkMode ? "bg-midnight text-gray-300" : "bg-neutral-100"
       }`}>
       
     <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <Header />
       </div>
       <div className="md:col-span-2 row-span-4">
-        <Card> Chart </Card>
+        <Chart/>
       </div>
       
       
