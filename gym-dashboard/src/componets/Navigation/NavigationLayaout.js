9af3306ed62fddf6cfa206a16eef5f0033ed861c
Arthur <zinnurov_2012@mail.ru>
2022-07-27 20:58:34 +0100

    navigation deletion header

--- gym-dashboard/src/componets/Navigation/NavigationLayaout.js
+++ gym-dashboard/src/componets/Navigation/NavigationLayaout.js
@@ -2,13 +2,15 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 import Navigation from "./Navigation";
 import Header from "../Header";
+import Themecolon from "../Themecolon";
 
 const NavigationLayaout = () => {
   return (
     <>
       <Navigation />
-      <Header />
+     
       <Outlet />
+
     </>
   );
 };
