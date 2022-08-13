1cc0deb2710c0a146240bdf3801df20e2241a414
Arthur <zinnurov_2012@mail.ru>
2022-08-13 19:52:31 +0100

    Added mock designs for body analytics

--- gym-dashboard/src/componets/BodyAnalytics/Body.js
+++ gym-dashboard/src/componets/BodyAnalytics/Body.js
@@ -3,6 +3,8 @@ import Card from "../Card";
 import { useContext } from "react";
 import ThemeContext from "../../contexts/ThemeContext";
 import BodyHighliter from "./BodyHighliter";
+import BodyDescription from "./BodyDescription";
+import ChartsOverall from "./ChartsOverall";
 const Body = () => {
   const { darkMode } = useContext(ThemeContext);
   return (
@@ -11,17 +13,19 @@ const Body = () => {
         darkMode ? "bg-midnight " : "bg-neutral-100"
       }`}
     >
-      <div className="row-span-2 xss:row-span-4 xs:row-span-6 sm:row-span-5 xl:row-span-3">
-        <div className="h-full rounded-md relative p-4 border-2 bg-white border-neutral-200">
+      <div className="row-span-3 xss:row-span-4 xs:row-span-6 sm:row-span-5 md:row-span-3 lg:row-span-4  xl:row-span-3">
+        <div className="h-full rounded-md relative p-4 border-2 bg-white shadow-md dark:bg-gray-800 dark:border-gray-700">
           <BodyHighliter />
         </div>
       </div>
 
       <div className="row-span-2 xs:row-span-2 xl:row-span-3">
-        <Card />
+        <BodyDescription />
       </div>
       <div className="row-span-2 xs:row-span-2 xl:row-span-3">
-        <Card />
+        <Card>
+          <ChartsOverall />
+        </Card>
       </div>
     </div>
   );
--- gym-dashboard/src/componets/Card.js
+++ gym-dashboard/src/componets/Card.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import ThemeContext from "../contexts/ThemeContext";
 
-const Card = ({children}) => {
+const Card = ({ children }) => {
+  const { darkMode } = useContext(ThemeContext);
   return (
-    <div className = "w-full h-full rounded-md relative p-4 border-2 bg-white border-neutral-200">{children}</div>
-  )
-}
+    <div
+      className={`w-full h-full rounded-md  shadow-md relative p-4 border-2 ${
+        darkMode ? "bg-gray-800" : "bg-white "
+      } dark:border-gray-700`}
+    >
+      
+    
+      {children}
+    </div>
+  );
+};
 
-export default Card
+export default Card;
