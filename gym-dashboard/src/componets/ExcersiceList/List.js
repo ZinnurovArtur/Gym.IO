fd2733d409f4697fc3721513d14d8cda72db2fda
Arthur <zinnurov_2012@mail.ru>
2022-08-14 21:16:17 +0100

    Main container

---
+++ gym-dashboard/src/componets/ExcersiceList/List.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useContext } from "react";
+import ThemeContext from "../../contexts/ThemeContext";
+import MainContent from "./MainContent";
+import SearchBar from "./SearchBar";
+const List = () => {
+  const { darkMode } = useContext(ThemeContext);
+  return (
+    <div className="container mx-auto px-4">
+      <div className="my-14 h-auto w-96 rounded-lg bg-[#f2f9fb] p-4 transition-all md:w-full">
+        <SearchBar />
+        <div className="mt-3 flex items-center justify-between">
+          <p className="text-sm font-semibold text-[#545968]">Showing 291 excersices</p>
+          <div>
+            <div className="flex items-center gap-1">
+              <p className="mt-1 text-xs">Sort by</p>
+              <select className="h-6 w-20 cursor-pointer text-sm font-semibold outline-none">
+                <option className="hidden">Size</option>
+                <option>Date</option>
+                <option>Time</option>
+                <option>Length</option>
+                <option>None</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <MainContent />
+      </div>
+    </div>
+  );
+};
+
+export default List;
