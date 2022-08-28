99dd82a886f544a55d07f349808de00c1f713aff
Arthur <zinnurov_2012@mail.ru>
2022-08-28 22:00:19 +0100

    getting data from api

--- gym-dashboard/src/componets/ExcersiceList/MainContent.js
+++ gym-dashboard/src/componets/ExcersiceList/MainContent.js
@@ -1,6 +1,6 @@
-import React from "react";
 import Card from "./Card";
-
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const products = [
   {
@@ -45,22 +45,72 @@ const products = [
   },
   // More products...
 ];
+
 const MainContent = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const validateImages = (array) => {
+    let result;
+
+    for (let i = 0; i < array.length; i++) {
+      
+      if (Array.isArray(array[i].images) && !array[i].length) {
+        for (let j = 0; j < array[i].images.length; j++) {
+          if (array[i].images[j].is_main) {
+            array[i].image = array[i].images[j].image;
+           
+          }
+        }
+      }
+    }
+    return array;
+  };
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `https://wger.de/api/v2/exerciseinfo/?format=json`,
+          {
+            params: {
+              language: 2,
+              limit:20,
+              offset:40
+            },
+          }
+        );
+
+        setData(validateImages(response.data.results));
+
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <Card imageSrc={product.imageSrc} imageAlt={product.imageAlt} />
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {product.price}
-              </p>
-            </a>
-          ))}
+          {loading && <div>A moment please...</div>}
+          {error && (
+            <div>{`There is a problem fetching the post data - ${error}`}</div>
+          )}
+
+          {data &&
+            data.map(({ id, name, category, image }) => (
+              <a key={id} className="group">
+                <Card muscle={category.name} name={name} imageSrc={image} />
+              </a>
+            ))}
         </div>
       </div>
     </div>
